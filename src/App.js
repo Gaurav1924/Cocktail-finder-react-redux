73bfbaf723b075e9129a975a795d4c2f886a2fe1
Gaurav1924 <gauravkshatriya99@gmail.com>
2024-08-15 19:16:12 +0530

    route fixes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import { Routes, Route, Router } from "react-router-dom";
-import Layout from "./Components/Layout";
-import PageNotFound from "./Components/PageNotFound";
-import SearchBox from "./Components/SearchBox";
-import HomePage from "./Pages/HomePage";
-import ProductDetails from "./Pages/ProductDetails";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './Components/Layout';
+import PageNotFound from './Components/PageNotFound';
+import SearchBox from './Components/SearchBox';
+import HomePage from './Pages/HomePage';
+import ProductDetails from './Pages/ProductDetails';
 
 function App() {
   return (
-    <Router  basename="/Cocktail-website-react-redux">
+    <Router basename="/Cocktail-finder-react-redux">
       <Routes>
         <Route
           path="/"
           element={
-            <>
-              <Layout>
-                <SearchBox />
-                <HomePage />
-              </Layout>
-            </>
+            <Layout>
+              <SearchBox />
+              <HomePage />
+            </Layout>
           }
         />
         <Route path="/products/:id" element={<ProductDetails />} />
