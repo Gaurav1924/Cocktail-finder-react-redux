0ed97d0b1084fdddefb6b87472def83b6a898bfb
unknown <gauravkshatriya99@gmail.com>
2024-08-20 10:08:11 +0530

    addedReadme

---
+++ Cocktail-finder-react-redux/src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="mt-4 bg-dark text-white">
+      <div className="container p-2 text-center">
+        <h2>Design And Developed By : Gaurav </h2>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ Cocktail-finder-react-redux/src/Pages/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../App.css'; 
+
+const About = () => {
+  return (
+    <section id="about" className="section-about">
+      <h2>About Us</h2>
+      <p>
+        Welcome to our cocktail website! We offer a curated selection of cocktail recipes 
+        from around the world. Whether you are a mixology enthusiast or just looking for 
+        your next favorite drink, we have something for everyone.
+      </p>
+    </section>
+  );
+};
+
+export default About;
